6faf51e792dab75a071756e132b990b2c52e3abb
Seun Lee <sleet99@naver.com>
2022-06-11 23:46:40 +0900

    :hammer: Add : Development scripts
    PostEmotionRepository.java 수정
    
    Resolves:
    See also:

--- back/src/main/java/com/diary/back/repository/PostEmotionRepository.java
+++ back/src/main/java/com/diary/back/repository/PostEmotionRepository.java
@@ -1,4 +1,9 @@
 package com.diary.back.repository;
 
-public interface PostEmotionRepository {
+import com.diary.back.model.PostEmotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEmotionRepository extends JpaRepository<PostEmotion,Long> {
 }
