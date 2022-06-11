5b792e4f0c5aa4a453327a778b55be3c50ce5705
Seun Lee <sleet99@naver.com>
2022-06-12 00:22:47 +0900

    :hammer: Add : Development scripts
    modified:   back/src/main/java/com/diary/back/repository/EmotionCategoryRepository.java
    modified:   back/src/main/java/com/diary/back/repository/EmotionRepository.java
    modified:   back/src/main/java/com/diary/back/repository/PostEmotionRepository.java
    modified:   back/src/main/java/com/diary/back/repository/PostRepository.java
    modified:   back/src/main/java/com/diary/back/repository/UserRepository.java
    
    Resolves:
    See also:

--- back/src/main/java/com/diary/back/repository/EmotionRepository.java
+++ back/src/main/java/com/diary/back/repository/EmotionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmotionRepository extends JpaRepository<Emotion,Long> {
+public interface EmotionRepository extends JpaRepository<Emotion, Long> {
 }
--- back/src/main/java/com/diary/back/repository/UserRepository.java
+++ back/src/main/java/com/diary/back/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.diary.back.repository;
 
-public interface UserRepository {
+import com.diary.back.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
