fd61ee01a920f20994e5a3c6a3f14471d8010828
Seun Lee <sleet99@naver.com>
2022-06-11 23:42:15 +0900

    :hammer: Add : Development scripts
    EmotionRepository.java 수정
    
    Resolves:
    See also:

--- back/src/main/java/com/diary/back/repository/EmotionRepository.java
+++ back/src/main/java/com/diary/back/repository/EmotionRepository.java
@@ -1,4 +1,9 @@
 package com.diary.back.repository;
 
-public interface EmotionRepository {
+import com.diary.back.model.Emotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmotionRepository extends JpaRepository<Emotion,Long> {
 }
