d1799f455f042ffdf5289a924116910214ca87e7
Seun Lee <sleet99@naver.com>
2022-06-11 23:49:41 +0900

    :hammer: Add : Development scripts
    PostRepository.java 수정
    
    Resolves:
    See also:

--- back/src/main/java/com/diary/back/repository/PostRepository.java
+++ back/src/main/java/com/diary/back/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.diary.back.repository;
 
-public interface PostRepository {
+import com.diary.back.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
 }
