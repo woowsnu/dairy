95288c29011360cfec24c0935f9db2076d872dc3
Seun Lee <sleet99@naver.com>
2022-06-12 22:08:51 +0900

    :hammer: Feat : Add development scripts to UserServiceImpl
    UserServiceImpl에 table 조회 및 UPDATE코드 추가
    
    Resolves: #25
    See also:

--- back/src/main/java/com/diary/back/service/UserServiceImpl.java
+++ back/src/main/java/com/diary/back/service/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.diary.back.service;
 
-public class UserServiceImpl {
+import com.diary.back.model.User;
+import com.diary.back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+
+    public List<User> update(User user){
+
+        final Optional<User> foundUser = repository.findById(user.getUser_id());
+
+        foundUser.ifPresent(newUser -> {
+            newUser.setUser_name(user.getUser_name());
+            newUser.setUser_email(user.getUser_email());
+            newUser.setUser_nickname(user.getUser_nickname());
+            newUser.setUser_password(user.getUser_password());
+            newUser.setUser_profile(user.getUser_profile());
+
+            repository.save(newUser);
+
+        });
+
+        return repository.findAll();
+
+    }
+
 }
