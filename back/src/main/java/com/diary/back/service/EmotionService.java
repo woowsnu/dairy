6f32b1ed51066a2281e3bf6fc5a0f2848070cafb
Seun Lee <sleet99@naver.com>
2022-06-12 20:23:50 +0900

    :hammer: Feat : Add development scripts to EmotionService, EmotionServiceImpl
    EmotionService, EmotionServiceImpl 개발코드추가
    
    EmotionService - Method 'findAll()' is never used
    EmotionServiceImpl - Private field 'repository' is never assigned
    
    EmotionService에 findAll이 사용되지않아서 경고가 뜨고
    EmotionServiceImpl에repository도 할당되지않았는데큰문제는 아닌듯
    
    Resolves:
    See also: #24

--- back/src/main/java/com/diary/back/service/EmotionService.java
+++ back/src/main/java/com/diary/back/service/EmotionService.java
@@ -1,4 +1,10 @@
 package com.diary.back.service;
 
+import com.diary.back.model.Emotion;
+
+import java.util.List;
+
 public interface EmotionService {
+
+    List<Emotion> findAll();
 }
--- back/src/main/java/com/diary/back/service/EmotionServiceImpl.java
+++ back/src/main/java/com/diary/back/service/EmotionServiceImpl.java
@@ -1,4 +1,20 @@
 package com.diary.back.service;
 
-public class EmotionServiceImpl {
+import com.diary.back.model.Emotion;
+import com.diary.back.repository.EmotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmotionServiceImpl implements EmotionService {
+
+    @Autowired
+    private EmotionRepository repository;
+
+    @Override
+    public List<Emotion> findAll(){
+        return repository.findAll();
+    }
 }
