a7d47d76e2b1792ca82781f337d884652f9b83da
Seun Yi <Yiseunn@gmail.com>
2022-06-13 15:39:26 +0900

    :hammer: Feat : Add development scripts to PostEmotion, PostEmotionService, PostEmotionServiceImpl
    PostEmotionServiceImpl에 findAll() 메서드 추가
    
    Resolves:
    See Also:

--- back/src/main/java/com/diary/back/service/PostEmotionServiceImpl.java
+++ back/src/main/java/com/diary/back/service/PostEmotionServiceImpl.java
@@ -1,4 +1,17 @@
 package com.diary.back.service;
 
-public class PostEmotionServiceImpl{
+import com.diary.back.model.PostEmotion;
+import com.diary.back.repository.PostEmotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PostEmotionServiceImpl implements PostEmotionService{
+
+    @Autowired
+    private PostEmotionRepository repository;
+
+    public List<PostEmotion> findAll(){
+        return repository.findAll();
+    }
 }
