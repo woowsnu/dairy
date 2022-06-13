cb6742b0b18eb8e3b3e07e9e9436f2ce3b274831
Seun Yi <Yiseunn@gmail.com>
2022-06-13 13:05:44 +0900

    :art: Feat : Improve structure code to Emotion, Post, PostEmotion model
    /model 패키지에서
    Emotion,Post,PostEmotion Joincolumn,ManyToOne 어노테이션 추가
    
    Resolves:
    See also:

--- back/src/main/java/com/diary/back/model/Post.java
+++ back/src/main/java/com/diary/back/model/Post.java
@@ -3,10 +3,7 @@ package com.diary.back.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
@@ -23,6 +20,8 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long post_id;
 
+    @ManyToOne
+    @JoinColumn(name="user_id")
     private Long user_id;
     private LocalDate post_date;
     private String post_text;
--- back/src/main/java/com/diary/back/model/PostEmotion.java
+++ back/src/main/java/com/diary/back/model/PostEmotion.java
@@ -3,10 +3,7 @@ package com.diary.back.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
 @RequiredArgsConstructor
@@ -19,9 +16,14 @@ public class PostEmotion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long post_emotion_id;
-
+    @ManyToOne
+    @JoinColumn(name="post_id")
     private Long post_id;
     private String emotion_word;
+    @ManyToOne
+    @JoinColumn(name="emotion_word_id")
     private Long emotion_word_id;
+    @ManyToOne
+    @JoinColumn(name="emotion_cat_id")
     private Long emotion_cat_id;
 }
