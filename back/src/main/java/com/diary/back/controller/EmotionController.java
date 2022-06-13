71db13315916db800511c4ac26d968ab95ab2b60
Thovy <kingvely151113@gmail.com>
2022-06-13 21:46:41 +0900

    :hammer: Feat : Add test to EmotionController

--- back/src/main/java/com/diary/back/controller/EmotionController.java
+++ back/src/main/java/com/diary/back/controller/EmotionController.java
@@ -1,8 +1,11 @@
 package com.diary.back.controller;
 
+import com.diary.back.model.Emotion;
 import com.diary.back.service.EmotionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileInputStream;
@@ -14,18 +17,5 @@ public class EmotionController {
 
     @Autowired
     private EmotionService emotionService;
-
-    // 파일 불러오기
-    public static Workbook upload(){
-        try{
-            String filePath = "../../../../../../../../감정단어 정리_20220613.xlsx";
-            FileInputStream fileStream = null;
-
-            fileStream = new FileInputStream(filePath);
-
-        } catch (FileNotFoundException e){
-            throw new RuntimeException(e.getMessage(), e);
-        }
-    }
 
 }
