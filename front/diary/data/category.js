b35a7a29620bc267371bcf0a05dcd656f4efeadf
woowsnu <woowsnu@gmail.com>
2022-10-10 00:09:54 +0900

    feat : 감정 단어 json-server 관리로 수정

---
+++ front/diary/data/category.js
@@ -0,0 +1,10 @@
+export const EMOTION_CAT = [
+	{ id: 1, name: '슬픔', color: '#122792' },
+	{ id: 2, name: '기쁨', color: '#FF993A' },
+	{ id: 3, name: '놀람', color: '#46E5D2' },
+	{ id: 4, name: '공포', color: '#7048E7' },
+	{ id: 5, name: '분노', color: '#FF4E49' },
+	{ id: 6, name: '혐오', color: '#3DB97F' },
+	{ id: 7, name: '지루함', color: '#4BAAEE' },
+	{ id: 8, name: '흥미', color: '#FF8DA9' },
+];
