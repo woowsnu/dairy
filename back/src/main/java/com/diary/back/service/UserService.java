aaaa9c6d12e89fbf04e8c1a1effd0c2a49c33970
thovy <98632452+thovy@users.noreply.github.com>
2022-10-20 12:01:13 +0900

    refactor: TEST 코드를 이용한 User create TEST
    TEST 코드를 이용해 USER create 를 TEST 했습니다. 성공적입니다.

--- back/src/main/java/com/diary/back/service/UserService.java
+++ back/src/main/java/com/diary/back/service/UserService.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface UserService {
 
+    User saveUser(User user);
 
 //    List<User> findbyid(User user);
 
--- back/src/test/java/com/diary/back/BackApplicationTests.java
+++ back/src/test/java/com/diary/back/BackApplicationTests.java
@@ -1,10 +1,44 @@
 package com.diary.back;
 
+import com.diary.back.DTO.UserDTO;
+import com.diary.back.model.User;
+import com.diary.back.service.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class BackApplicationTests {
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public User createUser(){
+		UserDTO userDTO = UserDTO.builder()
+				.username("usernameTest")
+				.email("emailTest")
+				.password("passwordTest")
+				.nickname("nickTest")
+				.build();
+		return User.createUser(userDTO, passwordEncoder);
+	}
+
+	@Test
+	@DisplayName("회원가입 테스트")
+	public void saveUserTest(){
+		User user = createUser();
+		User saveUser = userService.saveUser(user);
+
+		assertEquals(user.getEmail(), saveUser.getEmail());
+	}
 
 	@Test
 	void contextLoads() {
