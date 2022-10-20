4a9ea79baf2f12dacf2ecca0e86d9f42f1e504b4
thovy <98632452+thovy@users.noreply.github.com>
2022-10-21 02:17:56 +0900

    test: POST 작성 TEST 코드 성공
    POST 작성 TEST 코드를 작성해 TEST 에 성공했습니다. emotion 은 미리 database 에 넣어놓고 database 에 있는 emotion 을 사용해야 test 가 제대로 동작합니다. createdate 와 updatedate 는 없어도 무방합니다.

--- back/src/test/java/com/diary/back/BackApplicationTests.java
+++ back/src/test/java/com/diary/back/BackApplicationTests.java
@@ -1,7 +1,11 @@
 package com.diary.back;
 
+import com.diary.back.DTO.PostDTO;
 import com.diary.back.DTO.UserDTO;
+import com.diary.back.model.Emotion;
+import com.diary.back.model.Post;
 import com.diary.back.model.User;
+import com.diary.back.service.PostService;
 import com.diary.back.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +42,29 @@ class BackApplicationTests {
 		User saveUser = userService.registUser(user);
 
 		assertEquals(user.getEmail(), saveUser.getEmail());
+	}
+
+	@Autowired
+	PostService postService;
+
+	Emotion emotion = new Emotion(0L,1L,"감정test1",2,3,4);
+	
+	public Post createPost(){
+		User user = createUser();
+		PostDTO postDTO = PostDTO.builder()
+				.title("postTitleTEST")
+				.contents("postContentsTEST")
+				.user(user)
+				.emotion(emotion)
+				.build();
+		return Post.createPost(postDTO);
+	}
+
+	@Test
+	@DisplayName("게시글 작성 테스트")
+	public void savePostTest(){
+		Post post = createPost();
+		Post savePost = postService.registPost(post);
 	}
 
 	@Test
