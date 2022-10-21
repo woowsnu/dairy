e6b4530af32c85936b99fa57722cc5f1888390ca
thovy <98632452+thovy@users.noreply.github.com>
2022-10-21 17:14:32 +0900

    fix: create 권한이 없던 user error 해결
    security 에서 create 권한이 없어 생기던 403 에러를 해결했습니다. 하지만 csrf 를 disable 로 해놓았기 때문에 추후에 token 이나 다른 방법을 통해 조금 더 보안에 신경써야합니다. 지금 당장은 로컬에서만 만지니 상관 없겠지만.

--- back/src/main/java/com/diary/back/config/SecurityConfig.java
+++ back/src/main/java/com/diary/back/config/SecurityConfig.java
@@ -14,7 +14,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/api/**")
+                .permitAll();
     }
 
     @Bean
